8b82aa963a7f2332aa0445b985c99c3692d238aa
John Doe <johndoe@example.com>
2024-04-13 20:04:21 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+# URL de l'API que vous souhaitez interroger
+url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+# Envoi d'une requête GET à l'URL spécifiée
+response = requests.get(url)
+
+# Vérification du code de statut de la réponse
+if response.status_code == 200:
+    # Si la requête est réussie (code de statut 200), vous pouvez accéder au contenu de la réponse
+    data = response.json()  # Convertir la réponse JSON en un objet Python
+    print(data)  # Afficher les données de la réponse
+else:
+    # Si la requête échoue, afficher un message d'erreur avec le code de statut
+    print('La requête a échoué avec le code de statut:', response.status_code)
